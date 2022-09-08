81970f3e9e8ca295d33f5ff9ccbecbd62ae587ca
okwelume45 <alexoxais@gmail.com>
2022-09-08 14:43:37 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
+
 #include <unistd.h>
 
+
+
 /**
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints "and that piece of art is useful" - Dora Korpar, 2015-10-19
-* Return: 1
-*/
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+			return (1);
+
 }
