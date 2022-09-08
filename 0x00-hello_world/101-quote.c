7e33cf45a193567ef85aabc8cb2c5e9c6d4dc9a0
okwelume45 <alexoxais@gmail.com>
2022-09-08 14:53:35 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
-
 #include <unistd.h>
 
-
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+* main - print the string in the function
+*
+* Return: 0
+*/
 int main(void)
-
 {
-
-		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-			return (1);
-
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
