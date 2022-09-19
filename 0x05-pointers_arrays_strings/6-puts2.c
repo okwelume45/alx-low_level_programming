3394ebd4a584b316516ef45b023e2654c5ceae86
okwelume45 <111152551+okwelume45@users.noreply.github.com>
2022-09-19 21:00:15 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * puts2 - prints every other character of a string
  * @s: A pointer to a char that will be changed
  *
  * Return: void which means our answer is correct
  */
-
 void puts2(char *s)
 {
 	int i;
