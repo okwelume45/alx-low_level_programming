a15842df6e8a4955154153fc39151b1d6421e4b7
okwelume45 <111152551+okwelume45@users.noreply.github.com>
2022-09-19 21:14:31 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,20 @@
-#include "main.h"
-/**
- * puts2 - prints every other character of a string
- * @s: A pointer to a char that will be changed
- *
- * Return: void which means our answer is correct
- */
-void puts2(char *s)
-{
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i = i + 2;
-	}
-	_putchar('\n');
-}
+#include "main.h"                                                                                                                                             
+/**                                                                                                                                                           
+ * puts2 - prints every other character of a string                                                                                                           
+ * @s: A pointer to a char that will be changed                                                                                                               
+ *                                                                                                                                                            
+ * Return: void which means our answer is correct                                                                                                             
+ */                                                                                                                                                           
+void puts2(char *s)                                                                                                                                           
+{                                                                                                                                                             
+        int i = 0;                                                                                                                                            
+                                                                                                                                                              
+        while (*(str + i) != '\0')                                                                                                                            
+        {                                                                                                                                                     
+                                                                                                                                                              
+                if (i % 2 == 0)                                                                                                                               
+                        putchar(*(str + i));                                                                                                                  
+                i++;                                                                                                                                          
+        }                                                                                                                                                     
+        _putchar(10);                                                                                                                                         
+}              
