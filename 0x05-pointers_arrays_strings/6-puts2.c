d5d2d1384abb3302cb6fa96f18fe832682383a11
okwelume45 <111152551+okwelume45@users.noreply.github.com>
2022-09-19 21:39:09 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,11 +11,11 @@ void puts2(char *s)
         int a;                                                                                                                                                
                                                                                                                                                               
         for (a = 0; s[a] != '\0'; a++)                                                                                                                        
-        {    
-            if (a % 2 == 0)                                                                                                                               
-            {                                                                                                                                             
-             _putchar(s[a]);                                                                                                                               
-            }                                                                                                                                             
+        {                                                                                                                                                     
+                if (a % 2 == 0)                                                                                                                               
+                {                                                                                                                                             
+                _putchar(s[a]);                                                                                                                               
+                }                                                                                                                                             
         }                                                                                                                                                     
         _putchar('\n');                                                                                                                                       
-}      
+}               
