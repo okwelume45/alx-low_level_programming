382e8b2f1096224df043dad35cf0479f679ee444
okwelume45 <alexoxais@gmail.com>
2022-09-19 13:13:13 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,13 @@
 
 void puts2(char *s)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-	while (s[i] != '\0')
+	while (*(str + i) != '\0')
 	{
-		_putchar(s[i]);
-		i = i + 2;
+		if (i % 2 == 0)
+			putchar(*(str + i));
+		i++;
 	}
-	_putchar('\n');
+	_putchar(10);
 }
