9269a06a71e5a2eb35955784bc08dc8d1a44d964
okwelume45 <111152551+okwelume45@users.noreply.github.com>
2022-09-19 21:35:54 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,21 @@
 #include "main.h"                                                                                                                                             
+                                                                                                                                                              
 /**                                                                                                                                                           
  * puts2 - prints every other character of a string                                                                                                           
  * @s: A pointer to a char that will be changed                                                                                                               
  *                                                                                                                                                            
  * Return: void which means our answer is correct                                                                                                             
  */                                                                                                                                                           
-void puts2(char *s)
-{
-	int a;
-
-	for (a = 0; s[a] != '\0'; a++)
-	{
-		if (a % 2 == 0)
-		{
-			_putchar(s[a]);
-		}
-	}
-	_putchar('\n');
-}     
+void puts2(char *s)                                                                                                                                           
+{                                                                                                                                                             
+        int a;                                                                                                                                                
+                                                                                                                                                              
+        for (a = 0; s[a] != '\0'; a++)                                                                                                                        
+        {                                                                                                                                                     
+                if (a % 2 == 0)                                                                                                                               
+                {                                                                                                                                             
+                _putchar(s[a]);                                                                                                                               
+                }                                                                                                                                             
+        }                                                                                                                                                     
+        _putchar('\n');                                                                                                                                       
+}      
