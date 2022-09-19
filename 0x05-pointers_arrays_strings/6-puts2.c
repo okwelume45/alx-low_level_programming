b3e05a3df2ea8f804af65e88b844f14c13da6851
okwelume45 <alexoxais@gmail.com>
2022-09-19 12:41:31 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @s: A pointer to a char that will be changed
+ *
+ * Return: void which means our answer is correct
+ */
+
+void puts2(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i = i + 2;
+	}
+	_putchar('\n');
+}
